1c40ca52e30268b6f15562bdd8101cc104e74ee3
Brad <lai.jiang@weimob.com>
2020-07-06 17:28:21 +0800

    主页面下拉菜单完成

--- src/components/dropdown-select/dropdown-select.js
+++ src/components/dropdown-select/dropdown-select.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 const ListItem = props => {
-  const list = props.list;
-  const Items = list.map((item, index) => 
-        <Dropdown.Item eventKey={item.id} key={item.id} active={item.checked ? true : false} >{item.name}</Dropdown.Item>
+  const [list, setList] = useState(props.list);
+  const [id, setId] = useState(0);
+  const setToggle = props.changeToggle;
+  
+  useEffect( () => {
+    let tmpList = list;
+    let toggle = '';
+    for(let i = 0; i < tmpList.length; i++) {
+      if(tmpList[i].id === id) {
+        tmpList[i].checked = true;
+        toggle = tmpList[i].name
+      }else {
+        tmpList[i].checked = false
+      }
+    }
+    
+    //改变菜单选中 包括头部和菜单内容
+    setList(Object.assign([], list, tmpList));
+    setToggle(toggle);
+  },[id])
+  
+  let Items = list && list.map((item, index) => 
+        <Dropdown.Item eventKey={item.id} key={item.id} active={item.checked ? true : false} onClick={() => setId(item.id)}>{item.name}</Dropdown.Item>
       );
   return (
     <>
@@ -15,7 +35,7 @@ const ListItem = props => {
 
 const DropdownButton = props => {
   const list = props.list;
-  const toggle = props.toggle;
+  const [toggle, setToggle] = useState(props.toggle);
   return (
     <Dropdown>
       <Dropdown.Toggle id="dropdown-basic" style={{"boxShadow": "none"}}>
@@ -23,7 +43,7 @@ const DropdownButton = props => {
       </Dropdown.Toggle>
 
       <Dropdown.Menu >
-        <ListItem list={list} />
+        <ListItem list={list} changeToggle={setToggle}/>
       </Dropdown.Menu>
     </Dropdown>
   )
--- src/main/mainList/head.js
+++ src/main/mainList/head.js
@@ -4,10 +4,10 @@ import {DropdownButton} from '../../components/dropdown-select';
 
 
 export default () => {
-  const dropDownList = [{"name" : "全部状态", id : 1, checked: true},
-                        {"name" : "未开始", id : 2, checked: false},  
-                        {"name" : "竞拍中", id : 3, checked: false},
-                        {"name" : "已结束", id : 4, checked: false},
+  const dropDownList = [{"name" : "全部状态", id : 0, checked: true},
+                        {"name" : "未开始", id : 1, checked: false},  
+                        {"name" : "竞拍中", id : 2, checked: false},
+                        {"name" : "已结束", id : 3, checked: false},
                         ];
   const dropDownToggle = "全部状态";
   return (
